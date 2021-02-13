5d473ddd9b0bbbd7c061e5ce121e437e96e5da11
Alan <alancamargo50@gmail.com>
2021-02-13 00:49:04 -0300

    start project and create some sections

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '042029cf1499e727e177ff8c6972414c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '874fd3ea-279b-4aa8-b42a-33c148a42dc9'
+};
